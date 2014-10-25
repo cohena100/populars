ff32be08d88d688f4471afba77997f8830c663fd
Avi Cohen <cohena100@gmail.com>
2014-10-25 10:45:13 +0300

    Initial Commit

---
+++ Populars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Populars
+//
+//  Created by Avi Cohen on 10/25/14.
+//  Copyright (c) 2014 Avi Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Populars/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Populars
+//
+//  Created by Avi Cohen on 10/25/14.
+//  Copyright (c) 2014 Avi Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
